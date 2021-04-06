5540a19877ac8a87d183dfdcb651910584d08b80
kushikushi5969 <yuya.kushida1221@gmail.com>
2021-04-06 12:21:52 +0900

    ファイルアップロード

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -3,17 +3,38 @@ import Rect from './Rect.jsx'
 
 export default class App extends React.Component {
 
-    // render () {
-    //     return <h1>Hello, world!</h1>
-    // }
+    constructor (props) {
+      super(props)
+      // creatRefでref変数を作成
+      this.upload = React.createRef()
+      this.done = React.createRef()
+    }
+
+    // アップロードされたファイルの処理
+    handleUpload = (e) => {
+      const file = e.target.files[0]
+      const reader = new FileReader()
+      reader.readAsText(file, 'UTF-8')
+      reader.onload = e => {
+        alert(e.target.result)
+      }
+      e.target.value = null
+
+      // currentプロパティ経由で生のHTMLElementを操作できる
+      this.done.current.style.display = ''
+    }
 
     render () {
       return (
         <>
+          <h1>Hello, world!</h1>
           <Rect />
           <Rect num={1} bgcolor='#e02020' />
           <Rect num={2} bgcolor='#20e020' />
           <Rect num={3} bgcolor='#2020e0' />
+          <input type='file' ref={this.upload} style={{display: 'none'}} onChange={this.handleUpload} />
+          <button onClick={() => this.upload.current.click()}>アップロード</button>
+          <div ref={this.done} style={{display: 'none'}}>アップロード完了</div>
         </>
       )
     }
